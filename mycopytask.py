2cb969a3eb58514f3e5e8895c594700d17ff3069
Sairasu11 <dumangcascapito@gmail.com>
2024-09-28 11:25:51 +0800

    mana ko sir

---
+++ mycopytask.py
@@ -0,0 +1,14 @@
+age = [-12,13,-14,15,-16]
+for data in age:
+    if data < 0:
+        print(str(data) + "negative")
+
+    if data > 0:
+        print(str(data) + "positive")
+
+username = ["juan", "pedro", "james", "sagaad", "fuego"]
+for datauser in username:  
+    print("james cute")
+    break
+else:
+    print("james none")
